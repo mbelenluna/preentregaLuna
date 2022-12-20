892eed4e0f3539dad0e2bd422a0746c32bc36011
mbelenluna <mariabelenluna18@gmail.com>
2022-12-20 18:25:36 -0300

    Cambios react router dom

--- src/App.js
+++ src/App.js
@@ -4,8 +4,19 @@ import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import List from './components/List';
+import { products } from "./data.js";
 
 function App() {
+
+  const getProducts = (categoryId) => {
+    return new Promise ((resolve) => {
+        setTimeout(() => {
+            resolve(
+                categoryId ? products.filter((product) => product.category === categoryId) : products
+            );
+        })
+    })
+}
   
   return (
     <div className="App">
